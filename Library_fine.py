b45d75e25396785716dd2eabebe4620e4c7b6eef
Joy Das <35962760+joydas65@users.noreply.github.com>
2018-11-29 00:44:41 +0530

    Create Library_fine.py

---
+++ Library_fine.py
@@ -0,0 +1,19 @@
+d1,m1,y1 = map(int, input().split())
+
+d2,m2,y2 = map(int, input().split())
+
+if y1 > y2:
+    
+    print("10000")
+    
+elif y1 == y2 and m1 > m2:
+    
+    print((m1 - m2) * 500)
+    
+elif y1 == y2 and m1 == m2 and d1 > d2:
+    
+    print((d1 - d2) * 15)
+    
+else:
+    
+    print("0")
