41df6a217b67178661bc7054449316472ab2de43
Joy Das <35962760+joydas65@users.noreply.github.com>
2019-02-01 23:35:26 +0530

    Create Game_of_Stones.py

---
+++ Game_of_Stones.py
@@ -0,0 +1,13 @@
+for _ in range(int(input())):
+    
+    n = int(input())
+    
+    k = n % 7
+    
+    if k == 0 or k == 1:
+        
+        print("Second")
+        
+    else:
+        
+        print("First")
