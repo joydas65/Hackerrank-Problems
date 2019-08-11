4e3827649d323f0d4c86c8a569310faaeae8a75e
Joy Das <35962760+joydas65@users.noreply.github.com>
2019-08-11 16:27:49 +0530

    Create Permuting_Two_Arrays.py

---
+++ Permuting_Two_Arrays.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the twoArrays function below.
+def twoArrays(k, A, B):
+
+    A = sorted(A)
+
+    B = sorted(B, reverse = True)
+
+    for i in range(n):
+
+        if A[i]+B[i] < k:
+
+            return "NO"
+
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    q = int(input())
+
+    for q_itr in range(q):
+        nk = input().split()
+
+        n = int(nk[0])
+
+        k = int(nk[1])
+
+        A = list(map(int, input().rstrip().split()))
+
+        B = list(map(int, input().rstrip().split()))
+
+        result = twoArrays(k, A, B)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
